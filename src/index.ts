82f9f9dfa776cbd48daa04168a25d403db22c0f1
yamaimo <yarnaimo@gmail.com>
2019-07-01 11:43:43 +0000

    rain 0.3, peer

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export { dayjs, t, tr, tt } from '@yarnaimo/rain'
 export { blue } from './blue'
-export { DayjsFromFirestoreTimestamp } from './types/DayjsFromFirestoreTimestamp'
+export {
+    DayjsFromFirestoreTimestamp,
+} from './types/DayjsFromFirestoreTimestamp'
 export { DocumentReference } from './types/DocumentReference'
 export { FieldValue } from './types/FieldValue'
--- src/types/DayjsFromFirestoreTimestamp.ts
+++ src/types/DayjsFromFirestoreTimestamp.ts
@@ -1,5 +1,4 @@
-import { t } from '@yarnaimo/rain'
-import day, { Dayjs, isDayjs } from 'dayjs'
+import { t, dayjs } from '@yarnaimo/rain'
 import { firestore } from 'firebase'
 import { admin } from '../firestore-admin'
 
@@ -7,15 +6,15 @@ const isTimestamp = (u: unknown): u is firestore.Timestamp | admin.Timestamp =>
     typeof u === 'object' && u !== null && 'seconds' in u && 'toMillis' in u
 
 export const DayjsFromFirestoreTimestamp = new t.Type<
-    Dayjs,
+    dayjs.Dayjs,
     firestore.Timestamp | admin.Timestamp,
     unknown
 >(
     'DayjsFromFirestoreTimestamp',
-    isDayjs,
+    dayjs.isDayjs,
     (u, c) => {
         if (isTimestamp(u)) {
-            const d = day(u.toDate())
+            const d = dayjs(u.toDate())
             return !d.isValid() ? t.failure(u, c) : t.success(d)
         } else {
             return t.failure(u, c)
